5508dba397df8012cf2824ed26bc57d5a87e7ad5
Harison Desire <99317510+Harison07@users.noreply.github.com>
2022-04-05 13:19:13 +0200

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://harison07.github.io/CI_AirFortServices/';
+$config['base_url'] = 'https://CI_AirFortServices/';
 
 /*
 |--------------------------------------------------------------------------
